102048fc6e71a83223b1e03036a71231e5a07f6f
kaola526 <huweixiong@storswift.com>
2023-06-08 22:07:39 +0800

    privte_key add decryptrecords

--- wasm/src/account/private_key.rs
+++ wasm/src/account/private_key.rs
@@ -16,6 +16,7 @@
 
 use crate::{
     account::{Address, PrivateKeyCiphertext, Signature, ViewKey},
+    record::RecordCiphertext,
     types::{CurrentNetwork, Encryptor, Environment, FromBytes, PrimeField, PrivateKeyNative, ToBytes},
 };
 
@@ -23,9 +24,48 @@ use core::{convert::TryInto, fmt, ops::Deref, str::FromStr};
 use rand::{rngs::StdRng, SeedableRng};
 use wasm_bindgen::prelude::*;
 
+use aleo_rust::{Network, Plaintext, Record};
+use serde::{Deserialize, Serialize};
+
 #[wasm_bindgen]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct PrivateKey(PrivateKeyNative);
+
+#[derive(Serialize)]
+pub struct OldRecordData<N: Network> {
+    record: Record<N, Plaintext<N>>,
+    transactionid: N::TransitionID,
+    serial_number: aleo_rust::Field<aleo_rust::Testnet3>,
+}
+
+#[derive(Serialize)]
+pub struct RecordData<N: Network> {
+    record: Record<N, Plaintext<N>>,
+    serial_number: String,
+    program_id: String,
+    height: u32,
+    timestamp: i64,
+    block_hash: String,
+    transaction_id: String,
+    transition_id: String,
+    function_name: String,
+    output_index: u8,
+    input: String,
+}
+
+#[derive(Deserialize)]
+pub struct RecordOrgData {
+    record_ciphertext: String,
+    program_id: String,
+    height: u32,
+    timestamp: i64,
+    block_hash: String,
+    transaction_id: String,
+    transition_id: String,
+    function_name: String,
+    output_index: u8,
+    input: String,
+}
 
 #[wasm_bindgen]
 impl PrivateKey {
@@ -105,6 +145,37 @@ impl PrivateKey {
         let private_key = Encryptor::decrypt_private_key_with_secret(ciphertext, secret)
             .map_err(|_| "Decryption failed".to_string())?;
         Ok(Self::from(private_key))
+    }
+
+    #[wasm_bindgen(js_name = "decryptrecords")]
+    pub fn decrypt_records(&self, recordstext: &str) -> Result<String, String> {
+        let record_org_datas: Vec<RecordOrgData> = serde_json::from_str(recordstext).unwrap_or_default();
+        let mut records = Vec::new();
+        for record_org in record_org_datas {
+            if let Ok(record) = RecordCiphertext::from_string(&record_org.record_ciphertext) {
+                if let Ok(plaintext) = record.decrypt(&ViewKey::from_private_key(&self)) {
+                    let program_id = record_org.program_id.clone();
+                    let record_name = &program_id[..program_id.len() - 5];
+                    if let Ok(serial_number) = plaintext.serial_number_string(&self, &program_id, record_name) {
+                        let record_data: RecordData<CurrentNetwork> = RecordData {
+                            record: plaintext.deref().clone(),
+                            serial_number,
+                            program_id,
+                            height: record_org.height,
+                            timestamp: record_org.timestamp,
+                            block_hash: record_org.block_hash,
+                            transaction_id: record_org.transaction_id,
+                            transition_id: record_org.transition_id,
+                            function_name: record_org.function_name,
+                            output_index: record_org.output_index,
+                            input: record_org.input,
+                        };
+                        records.push(record_data)
+                    };
+                };
+            };
+        }
+        Ok(serde_json::to_string_pretty(&records).unwrap_or_default().replace("\\n", ""))
     }
 }
 
